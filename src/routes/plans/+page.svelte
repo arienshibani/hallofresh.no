3f73d79c48a924e35e2bac878a04c5d9ce29cea9
Arien Shibani <aaari94@gmail.com>
2023-05-23 20:53:02 +0200

    chore: :construction: doing stuff

--- src/routes/plans/+page.svelte
+++ src/routes/plans/+page.svelte
@@ -1,7 +1,26 @@
 <script>
     // @ts-nocheck
 
-    import { Card, Listgroup, Avatar } from "flowbite-svelte";
+    import { Card, Listgroup, Avatar, Button } from "flowbite-svelte";
+
+    let count = 2
+    let price = 136.42
+
+    function handlePlus() {
+		count += 1;
+        if(count >= 10){
+            // Do not allow the counter to go above 10
+            count = 10
+        }
+	}
+
+    function handleMinus() {
+		count -= 1;
+        // Do not allow the counter to go below 1
+        if(count <= 1){
+            count = 1
+        }
+	}
 
     let meny = [
         {
@@ -29,90 +48,149 @@
     let ingredients = [
         {
             name: "Rødløk",
-            amount: "1 stk",
+            amount: 0.5,
+            measurement: "stk"
         },
         {
             name: "Agurk",
-            amount: "1stk",
+            amount: 0.5,
+            measurement: "stk"
         },
         {
             name: "Poteter",
-            amount: "6 stk",
+            amount: 3,
+            measurement: "stk"
         },
         {
             name: "Spagetti",
-            amount: "500g",
+            amount: 250,
+            measurement: "g"
         },
         {
             name: "Matfløte",
-            amount: "3dl",
+            amount: 1.5,
+            measurement: "dl"
         },
         {
             name: "Soltørkede Tomater",
-            amount: "190g",
+            amount: 100,
+            measurement: "g"
         },
         {
             name: "Sjampinjong",
-            amount: "250g",
+            amount: 125,
+            measurement: "g"
         },
         {
             name: "Kyllinglårfilet",
-            amount: "300g",
+            amount: 150,
+            measurement: "g"
         },
 
         {
             name: "Creme Fraiche",
-            amount: "300g",
+            amount: 150,
+            measurement: "g"
         },
         {
             name: "Cherry Tomater",
-            amount: "300g",
+            amount: 150,
+            measurement: "g"
         },
     ];
+
+    const handleMeasurementConversion = (amount, measurement) => {
+        let convertedMeasurement = {amount: amount, measurement: measurement}
+
+        if(measurement === "g"){
+            if(amount >= 1000){
+                convertedMeasurement.measurement = "kg"
+                convertedMeasurement.amount = amount / 1000
+            }
+            return convertedMeasurement
+        }
+
+        if(measurement === "kg"){
+            if(amount < 1){
+                convertedMeasurement.measurement = "g"
+                convertedMeasurement.amount = amount * 1000
+            }
+        }
+
+        return `${amount} ${measurement}`
+    } 
 </script>
 
 <div class="flex" />
 
-<div class="grid grid-cols-2 justify-items-center">
+<div class="grid mainContent gap-10 p-20">
 
-        <Card padding="xl" size="sm">
-            <div class="flex justify-between items-center mb-4">
-                <h5
-                    class="text-xl font-bold leading-none text-gray-900 dark:text-white"
-                >
-                    Retter
-                </h5>
-            </div>
+
+         <Card class="h-max justify-self-end">
+            <h5 class="text-md font-bold leading-none text-gray-900 dark:text-white pb-5">
+                Middager 🍝
+            </h5>
+
+
             <Listgroup
                 items={meny}
                 let:item
-                class="border-0 dark:!bg-transparent"
-            >
+                class="border-0 dark:!bg-transparent">
                 <div class="flex items-center space-x-4">
                     <div class="flex-1 min-w-0">
                         <a href={item.recipeUrl}>
                             <p class="text-sm font-medium text-gray-900 truncate dark:text-white">{item.dish}</p>
                         </a>
                        
-                            <p class="text-sm text-gray-500 truncate dark:text-gray-400">
-                                {item.subtitle}
-                            </p>
-                     
+                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                            {item.subtitle}
+                        </p>
                     </div>
                 </div>
             </Listgroup>
         </Card>
 
-            <Card padding="xl" size="md">
-            <h5 class="text-md font-bold leading-none text-gray-900 dark:text-white">
-                Ingredienser
+        <Card class="lastColumn h-max ">
+            <h5 class="text-md font-bold leading-none text-gray-900 dark:text-white pb-5">
+                Handleliste 🛒
             </h5>
-            <br />
+
+            <div>
+                <button class="border-2 rounded-full w-[1.8rem]" on:click={handleMinus}>-</button> 
+                <span class="p-10"> {count} Personer </span> 
+                <button class="border-2 rounded-full w-[1.9rem]" on:click={handlePlus}>+</button> 
+            </div>
+
+            
+
             <Listgroup items={ingredients} let:item class="border-0 dark:!bg-transparent">
                 {item.name}
-                <div class="inline-flex items-centertext-sm font-medium text-blue-600 hover:underline dark:text-blue-500" >
-                    {item.amount}
+                <div class="inline-flex items-centertext-sm font-medium " >
+                    {item.amount * count}{item.measurement}
                 </div>
             </Listgroup>
-        </Card>
+
+            <br>
+
+            <Button color="dark">{parseFloat((price * count).toFixed(2))} kr på Meny</Button>
+         </Card>
 </div>
+
+
+<style>
+    .lastColumn{
+        grid-column-end: none;
+    }
+
+    .mainContent{
+        grid-template-columns: repeat(2, minmax(0, 1fr));
+    }
+
+    @media (max-width: 650px) {
+    .mainContent{
+        justify-content: center;
+        grid-template-columns: none;
+    }
+
+}
+</style>
