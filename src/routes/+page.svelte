9a3d4134ef2be319171f8e553d3bcbd08e9ee72b
Arien Shibani <aaari94@gmail.com>
2023-05-23 16:12:05 +0200

    chore: :rocket:

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>hellafraiche</h1>
+
+<h2 class="ukemeny">Ukemeny</h2>
+<ul>
+    <li>Spaghetti</li>
+    <li>Matfløte</li>
+    <li>Soltørkede Tomater i Olje</li>
+    <li>Sjampinjong</li>
+    <li>Spinat</li>
+    <li>300g kyllinglårfillet</li>
+    <li>Rødløk</li>
+    <li>4-6 Poteter</li>
+    <li>Creme Fraiche</li>
+    <li>Cherrytomater</li>
+    <li>Agurk</li>
+</ul>
