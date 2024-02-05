dcb5dcdc375aba7fe619a3120ebb249a3a0790c4
Arien Shibani <aaari94@gmail.com>
2024-02-05 15:52:56 +0100

    feat(src/routes/plans/[mealPlanId]/+page.svelte): :sparkles: Add weekdays to the individual meals in a mealplan overview.

--- src/routes/plans/[mealPlanId]/+page.svelte
+++ src/routes/plans/[mealPlanId]/+page.svelte
@@ -1,164 +1,159 @@
 <script>
     // @ts-nocheck
     import {
-        ArrowLeftCircle,
-        ArrowRightCircle,
         PlusCircle,
         MinusCircle,
-    } from 'svelte-heros-v2';
-    import { goto } from '$app/navigation';
+    } from "svelte-heros-v2";
+    import { goto } from "$app/navigation";
 
-    import { 
-        Card, 
-        Listgroup, 
-        Checkbox 
-    } from "flowbite-svelte";
+    import { Card, Listgroup, Checkbox } from "flowbite-svelte";
 
     // Retrieve the recipes from the data.
     $: ({ mealPlan, nextMealPlanId, previousMealPlanId } = data);
     export let data;
-    console.log(data)
+    console.log(data);
 
-    let count = 2
-    let price = 136.42 // Todo: store in DB lol
+    let count = 2;
+    let price = 136.42; // Todo: store in DB lol
 
     function handlePlus() {
-		count += 1;
-        if(count >= 10){
+        count += 1;
+        if (count >= 10) {
             // Do not allow the counter to go above 10
-            count = 10
+            count = 10;
         }
-	}
+    }
 
     function handleMinus() {
-		count -= 1;
+        count -= 1;
         // Do not allow the counter to go below 1
-        if(count <= 1){
-            count = 1
+        if (count <= 1) {
+            count = 1;
         }
-	}
+    }
 
-const handleNextMealPlan = () => {
-    goto(`/plans/${data.nextMealPlanId}`)
-}
+    const handleNextMealPlan = () => {
+        goto(`/plans/${data.nextMealPlanId}`);
+    };
 
-const handlePreviousMealPlan = () => {
-    goto(`/plans/${data.previousMealPlanId}`)
-}
+    const handlePreviousMealPlan = () => {
+        goto(`/plans/${data.previousMealPlanId}`);
+    };
 
-
+    const ukedager = [
+        "Mandag",
+        "Tirsdag",
+        "Onsdag",
+        "Torsdag",
+        "Fredag",
+        "Lørdag",
+        "Søndag",
+    ];
 </script>
 
-
-
-
-
-
 <div class="grid mainContent gap-x-40 p-10">
-         <Card class="h-max justify-self-end border-none shadow-none">
-            <h1 class="text-slate-900 text-4xl text-center pt-16 pb-5">{mealPlan.name}</h1>
+    <Card class="h-max justify-self-end border-none shadow-none">
+        <h1 class="text-slate-900 text-4xl text-center pt-16 pb-5">
+            {mealPlan.name}
+        </h1>
 
-            <div class="flex justify-evenly">
-
-                <h2 class="text-2xl text-center text-gray-900"></h2>
-
-            </div>
-          
-            <Listgroup
-                items={mealPlan.recipes}
-                let:item
-                class="border-0 dark:!bg-transparent pt-5">
+        <Listgroup
+            items={mealPlan.recipes}
+            let:item
+            let:index
+            class="border-0 dark:!bg-transparent pt-5"
+        >
+            <a href="/recipes/{item.title}">
                 <div class="flex items-center space-x-4 mt-2.5">
                     <div class="flex-1 min-w-0">
-                        <a href="/recipes/{item.title}">
-                            <p class="text-sm font-medium text-gray-900 truncate dark:text-white">{item.title}</p>
-                        </a>
-                       
-                        <p class="text-sm text-gray-500 truncate dark:text-gray-400">
+                        <b>{ukedager[index]}</b>
+                        <p
+                            class="text-sm font-medium text-gray-900 truncate dark:text-white"
+                        >
+                            {item.title}
+                        </p>
+
+                        <p
+                            class="text-sm text-gray-500 truncate dark:text-gray-400"
+                        >
                             {item.subtitle}
                         </p>
                     </div>
                 </div>
-            </Listgroup>
-
-
-            <p class="text-center text-gray-900 mt-8 mb-4">Antall Personer</p>
-
-            <div class="flex justify-evenly m-1">
-                <button on:click={handleMinus}>  
-                    <MinusCircle/>
-                </button> 
-                <span class=""> {count} {count === 1 ? "Person" : "Personer"} </span> 
-                <button on:click={handlePlus}>
-                    <PlusCircle/>
-                </button> 
-            </div>
-
-            <h1 class="text-xl text-center pt-16 text-gray-950">Få varene levert på døra?</h1>
-
-
-
-
-            <a class="items-center self-center" target="_blank" href="{mealPlan.menyUrl}">
-                <button class="items-center self-center border border-black border-r-4 border-b-4  rounded-sm h-10 w-fit pr-4 pl-4 m-5" >
-                    {parseFloat((mealPlan.price * count).toFixed(2))} kr hos Meny
-                </button>
             </a>
+        </Listgroup>
 
-            <div class="flex justify-center">
-                <h2 class="text-xs text-center pt-4 pb-5 max-w-content">  Handlelisten blir forhåndsutfylt for 2 personer. Prisen er kun et estimat.</h2>
+        <p class="text-center text-gray-900 mt-8 mb-4">Antall Personer</p>
+
+        <div class="flex justify-evenly m-1">
+            <button on:click={handleMinus}>
+                <MinusCircle />
+            </button>
+            <span class="">
+                {count}
+                {count === 1 ? "Person" : "Personer"}
+            </span>
+            <button on:click={handlePlus}>
+                <PlusCircle />
+            </button>
+        </div>
+    </Card>
+
+    <Card class="lastColumn h-max shadow-none rounded-sm mt-12 mb-14">
+        <h5
+            class="text-md tracking-widest text-center leading-none text-gray-900 dark:text-white pb-5"
+        >
+            HANDLELISTE
+        </h5>
+        <Listgroup
+            items={mealPlan.ingredients}
+            let:item
+            class="border-0 dark:!bg-transparent"
+        >
+            {item.name}
+            <div class="">
+                <Checkbox class="m-2 checked:bg-slate-900 outline-black" />
+                {item.amount * count}{item.measurement}
             </div>
-
-        </Card>
-
-        <Card class="lastColumn h-max shadow-none rounded-sm mt-12 mb-14">
-            <h5 class="text-md tracking-widest  text-center leading-none text-gray-900 dark:text-white pb-5">
-                HANDLELISTE 
-            </h5>            
-            <Listgroup items={mealPlan.ingredients} let:item class="border-0 dark:!bg-transparent">
-                {item.name} 
-                <div class="">
-                    <Checkbox class="m-2 checked:bg-slate-900 outline-black"/> {item.amount * count}{item.measurement} 
-                </div>
-            </Listgroup>
-            <br>
-            <br>
-            {#if mealPlan.bulkIngredients.length !== 0}
-            <h5 class="text-md  tracking-widest text-center leading-none text-gray-900 dark:text-white pb-5">
+        </Listgroup>
+        <br />
+        <br />
+        {#if mealPlan.bulkIngredients.length !== 0}
+            <h5
+                class="text-md tracking-widest text-center leading-none text-gray-900 dark:text-white pb-5"
+            >
                 FRA EGET KJØKKENSKAP
             </h5>
-            <Listgroup items={mealPlan.bulkIngredients} let:item class="border-0 dark:!bg-transparent">
-                {item.name} 
+            <Listgroup
+                items={mealPlan.bulkIngredients}
+                let:item
+                class="border-0 dark:!bg-transparent"
+            >
+                {item.name}
             </Listgroup>
-            {/if}
-        </Card>
-
-        
-
-  
+        {/if}
+    </Card>
 </div>
 
-
 <style>
-    .lastColumn{
+    .lastColumn {
         grid-column-end: none;
     }
 
-    .mainContent{
+    .mainContent {
         grid-template-columns: repeat(2, minmax(0, 1fr));
     }
 
     @media (max-width: 650px) {
-    .mainContent{
-        justify-content: center;
-        grid-template-columns: none;
+        .mainContent {
+            justify-content: center;
+            grid-template-columns: none;
+        }
     }
 
-}
-
-  button:hover{
-    color: white;
-    background-color: black;
-    border-color: white;
-  }
+    button:hover {
+        color: white;
+        background-color: black;
+        border-color: white;
+    }
 </style>
