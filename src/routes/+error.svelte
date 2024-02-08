2e5388d87f272dfbfaeb35648d2b1e9703372ab8
Arien Shibani <aaari94@gmail.com>
2024-02-08 14:33:33 +0100

    feat(+error.svelte): :sparkles: Add a nice looking error page instead of ugly default

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+    import img from "$lib/assets/peep-wut.png";
+    import { List, Li } from "flowbite-svelte";
+</script>
+
+<h1 class="text-3xl text-center p-20 font-bold">
+    Beklager, denne siden finnes ikke..
+</h1>
+
+<img src={img} alt="A confused peep" class="w-1/4 mx-auto" />
+
+<div class="align-middle text-center w-80 m-auto">
+    <List list="disc" class="text-left">
+        <Li>
+            Gå tilbake til <a href="/" class="link text-cyan-500">hovedsiden?</a>
+        </Li>
+        <Li href="">
+            eller <a href="https://github.com/arienshibani/hallofresh.no/discussions" class="link text-cyan-500">kontakt oss</a>
+        </Li>
+    </List>
+</div>
